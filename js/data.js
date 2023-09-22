f74d639f38a0213428519c79a578168c2d1eb9cb
Andrew Roide <andrew.roide@hotmail.com>
2023-09-22 05:17:09 +0000

    added functions to render entry list, show either data-view, swap dataviews

--- js/data.js
+++ js/data.js
@@ -14,6 +14,61 @@ window.addEventListener('beforeunload', (event) => {
   localStorage.setItem('javascript-local-storage', dataModel)
 });
 
-if (previousData !== null) {
-  data = JSON.parse(previousData);
+document.addEventListener('DOMContentLoaded', function () {
+  if (previousData !== null) {
+    data = JSON.parse(previousData);
+    data.entries.forEach((entry) => {
+      renderEntry(entry);
+    });
+  } else {
+    const entryList = document.querySelector('.entry-list');
+    const newLi = document.createElement("li");
+    newLi.innerHTML = 'No entries to show';
+    newLi.classList.add('noEntries');
+    entryList.appendChild(newLi);
+  };
+});
+
+function renderEntry(entry) {
+  const entryList = document.querySelector('.entry-list');
+
+
+  const newLi = document.createElement("li");
+
+  newLi.innerHTML = '<li> <img src= "' + entry.url + '" alt = ""/> <div>Title ' + entry.title + '</div> <div>notes ' + entry.notes + '</div></li > ' ; // You can also set other properties like className, id, etc.
+
+  entryList.appendChild(newLi);
+}
+
+function toggleNoEntries() {
+  const noEntries = document.querySelector('.noEntries');
+  if (noEntries.classList.contains('hide')) {
+      noEntries.classList.remove('hide');
+  } else {
+      noEntries.classList.add('hide');
+  }
+}
+
+function viewSwap(elementToSwap) {
+  const entryForm = document.querySelector('[data-view="entry-form"]');
+  const entries = document.querySelector('[data-view="entries"]');
+
+  if (elementToSwap === 'entries') {
+    const previousData = localStorage.getItem('javascript-local-storage');
+    const dataModel = JSON.stringify(data);
+    dataModel.view = 'entries';
+    localStorage.setItem('javascript-local-storage', dataModel);
+
+    entries.classList.remove('hide');
+    entryForm.classList.add('hide');
+
+  } else {
+    const previousData = localStorage.getItem('javascript-local-storage');
+    const dataModel = JSON.stringify(data);
+    dataModel.view = 'entry-form';
+    localStorage.setItem('javascript-local-storage', dataModel);
+
+    entries.classList.add('hide')
+    entryForm.classList.remove('hdie');
+  }
 }
