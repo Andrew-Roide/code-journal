6e0e7f2042da2e27d685fe06d02271400ab7a456
Andrew Roide <andrew.roide@hotmail.com>
2023-09-26 03:35:33 +0000

    created dom tree, moved functions from data.js to main.js, added eventlisteners to entries and entry-form

--- js/data.js
+++ js/data.js
@@ -6,66 +6,13 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+var previousData = localStorage.getItem('javascript-local-storage');
+
+if (previousData !== null) {
+  data = JSON.parse(previousData);
+}
 
 window.addEventListener('beforeunload', (event) => {
   const dataModel = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataModel);
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  const previousData = localStorage.getItem('javascript-local-storage');
-
-  if (previousData !== null) {
-    data = JSON.parse(previousData);
-    data.entries.forEach((entry) => {
-      renderEntry(entry);
-    });
-    viewSwap(data.view);
-      if (data.entries.length > 0) {
-        toggleNoEntries();
-      }
-  } else {
-    viewSwap(data.view)
-  }
-});
-
-function renderEntry(entry) {
-  const entryList = document.querySelector('.entry-list');
-  const newLi = document.createElement("li");
-
-  newLi.innerHTML = '<li class="row entry-container"> <div class="column-half"><img class="img-preview" src= "' + entry.url + '"/></div> <div class="column-half entry-info-container"><div class="entry-title">' + entry.title + '</div> <div class="entry-notes">' + entry.notes + '</div></div></li > ' ;
-  entryList.appendChild(newLi);
-}
-
-function toggleNoEntries() {
-  const noEntries = document.querySelector('.noEntries');
-  if (noEntries.classList.contains('hidden')) {
-      noEntries.classList.remove('hidden');
-  } else {
-      noEntries.classList.add('hidden');
-  }
-}
-
-function viewSwap(elementToSwap) {
-  const entryForm = document.querySelector('[data-view="entry-form"]');
-  const entries = document.querySelector('[data-view="entries"]');
-
-  if (elementToSwap === 'entries') {
-      data.view = 'entries';
-
-    const dataModel = JSON.stringify(data);
-
-    localStorage.setItem('javascript-local-storage', dataModel);
-    entries.classList.remove('hidden');
-    entryForm.classList.add('hidden');
-
-  } else {
-    data.view = 'entry-form';
-
-    const dataModel = JSON.stringify(data);
-
-    localStorage.setItem('javascript-local-storage', dataModel);
-    entries.classList.add('hidden')
-    entryForm.classList.remove('hidden');
-  }
-}
--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const imgPreview = document.querySelector('.img-preview');
 const title = document.querySelector('#title');
 const photoURL = document.querySelector('#url');
 const notes = document.querySelector('#notes');
+const viewEntries = document.querySelector('.entries-link');
+const viewEntryForm = document.querySelector('.new');
 
 photoURL.addEventListener('input', (event) => {
   imgPreview.setAttribute('src', event.target.value);
@@ -23,8 +25,98 @@ form.addEventListener('submit', (event) => {
   form.reset();
   renderEntry(newObject);
   viewSwap('entries');
+});
 
-  if(data.entries.length > 0) {
-    toggleNoEntries();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const previousData = localStorage.getItem('javascript-local-storage');
+
+  if (previousData !== null) {
+    data = JSON.parse(previousData);
+    data.entries.forEach((entry) => {
+      renderEntry(entry);
+    });
+    viewSwap(data.view);
+    if (data.entries.length > 0) {
+      toggleNoEntries();
+    }
+  } else {
+    viewSwap(data.view)
   }
 });
+
+function renderEntry(entry) {
+  const entryList = document.querySelector('.entry-list');
+  const newLiContainer = document.createElement("li");
+
+  const newLi = document.createElement('li');
+  newLi.setAttribute('class', 'row entry-container');
+
+  const columnHalf = document.createElement('div');
+  columnHalf.setAttribute('class', 'column-half');
+
+  const imgPreview = document.createElement('img');
+  imgPreview.setAttribute('class', 'img-preview');
+  imgPreview.src = entry.url;
+
+  const entryInfoContainer = document.createElement('div');
+  entryInfoContainer.setAttribute('class', 'column-half entry-info-container');
+
+  const entryTitle = document.createElement('div');
+  entryTitle.setAttribute('class', 'entry-title');
+  entryTitle.textContent = entry.title;
+
+  const entryNotes = document.createElement('div');
+  entryNotes.setAttribute('class', 'entry-notes');
+  entryNotes.textContent = entry.notes;
+
+  columnHalf.appendChild(imgPreview);
+  entryInfoContainer.appendChild(entryTitle);
+  entryInfoContainer.appendChild(entryNotes);
+  newLi.appendChild(columnHalf);
+  newLi.appendChild(entryInfoContainer);
+  newLiContainer.appendChild(newLi);
+  entryList.appendChild(newLiContainer);
+}
+
+function toggleNoEntries() {
+  const noEntries = document.querySelector('.noEntries');
+
+  if (noEntries.classList.contains('hidden')) {
+    noEntries.classList.remove('hidden');
+  } else {
+    noEntries.classList.add('hidden');
+  }
+}
+
+viewEntries.addEventListener('click', () => {
+  viewSwap('entries');
+});
+
+viewEntryForm.addEventListener('click', () => {
+  viewSwap('entry-form');
+})
+
+function viewSwap(elementToSwap) {
+  const entryForm = document.querySelector('[data-view="entry-form"]');
+  const entries = document.querySelector('[data-view="entries"]');
+
+  if (elementToSwap === 'entries') {
+    data.view = 'entries';
+
+    const dataModel = JSON.stringify(data);
+
+    localStorage.setItem('javascript-local-storage', dataModel);
+    entries.classList.remove('hidden');
+    entryForm.classList.add('hidden');
+
+  } else {
+    data.view = 'entry-form';
+
+    const dataModel = JSON.stringify(data);
+
+    localStorage.setItem('javascript-local-storage', dataModel);
+    entries.classList.add('hidden')
+    entryForm.classList.remove('hidden');
+  }
+}
